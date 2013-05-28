cb313fb6297b6f7a8d87badfbf9c992110f50815
Marlon <marlon@scheid.net>
2013-05-28 22:47:14 +0200

    clock added to settings and rooms. added timer to settings

--- footer.php
+++ footer.php
@@ -9,9 +9,27 @@ $active = $_GET['page']
 		<ul>
 			<li><a href="?page=dashboard" <?php if($active == 'dashboard') { ?> class="ui-btn-active ui-state-persist" <?php } ?> id="dashboard1" data-icon="custom" rel="external">Dashboard</a></li>
 			<li><a href="?page=multimedia" <?php if($active == 'multimedia') { ?> class="ui-btn-active ui-state-persist" <?php } ?>  id="tv" data-icon="custom" rel="external">Multimedia</a></li>
-			<li><a href="?page=dreambox&id=start" <?php if($active == 'dreambox') { ?> class="ui-btn-active ui-state-persist" <?php } ?>  id="tv" data-icon="custom" rel="external">Dreambox</a></li>
-			<li><a href="?page=room&room=1" <?php if($active == 'room') { ?> class="ui-btn-active ui-state-persist" <?php } ?>  id="haus" data-icon="custom" rel="external">Räume</a></li>
-
+			<?
+			$sql = query( "SELECT value FROM config WHERE options='DreamBoxavail'");
+			$config = fetch( $sql);
+			if ($config['value'] == 'Yes'){
+				?>
+				<li><a href="?page=dreambox&id=start" <?php if($active == 'dreambox') { ?> class="ui-btn-active ui-state-persist" <?php } ?>  id="tv" data-icon="custom" rel="external">Dreambox</a></li>
+				<?
+			}
+			?>
+			
+			<?
+			//show rooms only if rooms available
+			$sql = query( "SELECT id FROM rooms");
+			$Rooms = fetch( $sql );	
+			if( $Rooms['id'] ){
+				?>
+				<li><a href="?page=room&room=1" <?php if($active == 'room') { ?> class="ui-btn-active ui-state-persist" <?php } ?>  id="haus" data-icon="custom" rel="external">Räume</a></li>
+				<?
+			}
+			?>
+			
 			<li><a href="?page=settings" <?php if($active == 'settings') { ?> class="ui-btn-active ui-state-persist" <?php } ?>  id="settings" data-icon="custom" rel="external">Einstellungen</a></li>
 		</ul>
 	</div><!-- /navbar -->
